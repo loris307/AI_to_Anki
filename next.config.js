fc1dab1aa26a9ab3d39a8b3628ce9c0783bb4198
Loris Galler <galler@campus.tu-berlin.de>
2025-07-16 10:34:11 +0200

    build regeln lol

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/anki',
+  assetPrefix: '/anki',
+  trailingSlash: true,
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    return config;
+  },
+  // Exclude Supabase functions from the build
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['./supabase/**/*'],
+    },
+  },
+};
 
 module.exports = nextConfig;
